f18afec8b79a1ed61fbc6c6204dee5cc6e734817
david alfia <davidal@edu.hac.ac.il>
2022-05-18 00:40:34 +0300

    add_test_simulation

--- junction_file_module.py
+++ junction_file_module.py
@@ -2,17 +2,11 @@ import openpyxl
 from junction import Junction
 from trafic_light import TrafficLight
 
-#cols numbers
+
 min_row = 2
-min_col = 2
 junction_name_col = 2
-num_of_lanes_col = 3
-cardinal_direction_col = 4
-red_time_col = 5
-green_time_col = 6
-traffic_light_number_col = 7
-next_junction_col = 8
-path_driving_time_col = 9
+traffic_light_name_col = 3
+
 
 list_of_junctions = []
 
@@ -24,21 +18,29 @@ def open_file():
 
 
 def get_info_from_xslx_file(sheet1):
-    for row in range(min_row, sheet1.max_row + 1):
+    pass
+
+
+
+    """for row in range(min_row, sheet1.max_row + 1):
         junction_name = sheet1.cell(row, junction_name_col).value
         number = sheet1.cell(row, 3).value
         j = Junction(junction_name, number, False)
         for i in range(4, 7):
-            hit_from = sheet1.cell(row, i).value
-            t = TrafficLight(i, 5, hit_from)
+            options = {
+                "strait": "south",
+                "right": "east",
+            }
+            t = TrafficLight(i, 5, 3,options)
             j.add_traffic_light(t)
         list_of_junctions.append(j)
-
+    """
 
 
 def extract_junction_info():
     sheet = open_file()
     get_info_from_xslx_file(sheet)
+
 
 try:
     extract_junction_info()
--- trafic_light.py
+++ trafic_light.py
@@ -1,11 +1,14 @@
+from queue import Queue
+from roadUser import RoadUser
+
+
 class TrafficLight:
-    def __init__(self, name, red_timer, hit_from, green_timer, options):
+    def __init__(self, name, red_timer, green_timer, **options):
         self.name = name
         self.red_timer = red_timer
-        self.green_timer = 1
-        self.hit_from = hit_from
-        self.traffic_light_directions = {}
-        self.analyze_directions_map(hit_from)
+        self.options = options
+        self.green_timer = green_timer
+        self.line = Queue()
 
     def get_curr_timer_red(self):
         return self.red_timer
@@ -20,14 +23,25 @@ class TrafficLight:
         self.green_timer = new_timer
 
     def get__cardinal_direction_by_turn_choice(self, turn_direction):
-        return self.traffic_light_directions[turn_direction]
+        return self.options[turn_direction]
 
-    def print_traffic_light(self):
-        print(f'traffic_light {self.name} has current stop light timer of {self.timer}'
-              f' and has directions: {self.traffic_light_directions}')
+    def __str__(self):
+        return (f'traffic_light {self.name} has current stop light timer of {self.red_timer} '
+                f'and has directions: {self.options}')
 
-    #def pop_from_traffic_light(self):
+    def pop_from_traffic_light(self,road_user):
+        self.line.put_nowait(road_user)
 
+    def push_to_traffic_light(self):
+        return self.line.get_nowait()
 
+    def get_options(self):
+        return self.options
 
-# t = TrafficLight("1", 8, left="north", right="south", straight="west")
+    def has_choice(self, turn):
+        if turn in self.options.keys():
+            return True
+        return False
+
+    def get_next_cd_by_turn(self, turn):
+        return self.options[turn]
